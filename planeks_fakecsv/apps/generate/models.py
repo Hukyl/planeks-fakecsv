6d24bc3ec221e9303d249e85bf685bee9ae1929a
Hukyl <a.shalaev7125@gmail.com>
2022-06-04 12:31:18 +0300

    Add schema model and update view

--- planeks_fakecsv/apps/generate/models.py
+++ planeks_fakecsv/apps/generate/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class DataSchema(models.Model):
+    """Data schema to generate fake CSV data according to it"""
+
+    class ColumnDelimiter(models.TextChoices):
+        """Column value separator in CSV file"""
+        COMMA = ',', 'Comma (,)'
+        PIPE = '|', 'Pipe (|)'
+        SEMICOLON = ';', 'Semicolon (;)'
+        TAB = '\t', 'Tab (\\t)'
+
+    class StringCharacter(models.TextChoices):
+        """String defining character in CSV file"""
+        SINGLE_QUOTE = '\'', 'Single-quote (\')'
+        DOUBLE_QUOTE = '"', 'Double-quote (")'
+
+    name = models.CharField(max_length=50, null=False, blank=False)
+    column_delimiter = models.CharField(
+        max_length=1, choices=ColumnDelimiter.choices, null=False,
+        blank=False, default=ColumnDelimiter.COMMA
+    )
+    string_character = models.CharField(
+        max_length=1, choices=StringCharacter.choices, null=False,
+        blank=False, default=StringCharacter.DOUBLE_QUOTE
+    )
+    last_modified_at = models.DateField(auto_now=True)
+    schema_columns = models.JSONField(null=False, blank=False)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE,
+        related_name='schemas', null=False
+    )
--- planeks_fakecsv/apps/generate/views.py
+++ planeks_fakecsv/apps/generate/views.py
@@ -1,6 +1,25 @@
+# pylint: disable=E1101
+
+from faker import Faker
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+
+from .models import DataSchema
+
+faker = Faker()
+CSV_DATA_TYPES = {
+    'Full name': faker.name,
+    'Job': faker.job,
+    'Domain name': faker.domain_name,
+    'Phone number': lambda: f'+{faker.msisdn()}',  # in GSM
+    'Company name': faker.company,
+    'Address': lambda: faker.address().replace('\n', '; '),
+    'Date': faker.date
+}
 
 
-def index(request):
-    return HttpResponse('<p>Hello World!</p>')
+def index(request: HttpRequest) -> HttpResponse:
+    schemas = DataSchema.objects.filter(user=request.user)
+    return render(
+        request, 'generate/index.html', context={'schemas': schemas}
+    )
