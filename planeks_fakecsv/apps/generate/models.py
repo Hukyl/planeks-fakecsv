9700809eac938fc4639a862cbe105f7b1ecd31b2
Hukyl <a.shalaev7125@gmail.com>
2022-06-04 14:15:15 +0300

    Add data set model and register in admin panel

--- planeks_fakecsv/apps/generate/models.py
+++ planeks_fakecsv/apps/generate/models.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -32,3 +34,31 @@ class DataSchema(models.Model):
         get_user_model(), on_delete=models.CASCADE,
         related_name='schemas', null=False
     )
+
+
+def user_directory_path(instance: DataSet, filename: str) -> str:
+    """
+    Upload path transformation for `DataSet.csv_file`
+
+    Args:
+        instance (DataSet)
+        filename (str)
+
+    Returns:
+        str
+    """
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f'user_{instance.schema.user.id}/{filename}'
+
+
+class DataSet(models.Model):
+    """
+    Data set, storing created csv file according to schema
+    """
+
+    schema = models.ForeignKey(
+        DataSchema, on_delete=models.CASCADE,
+        related_name='datasets', null=False
+    )
+    csv_file = models.FileField(upload_to=user_directory_path)
+    created_at = models.DateField(auto_now_add=True)
