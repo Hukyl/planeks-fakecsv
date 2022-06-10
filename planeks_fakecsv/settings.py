df37adc70617f1315bd0f3defc428bfd13c62f6a
Hukyl <a.shalaev7125@gmail.com>
2022-06-10 18:46:45 +0300

    Set up environment debug mode

--- planeks_fakecsv/settings.py
+++ planeks_fakecsv/settings.py
@@ -30,7 +30,7 @@ sys.path.insert(0, str(PROJECT_DIR / 'apps'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'true'
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
